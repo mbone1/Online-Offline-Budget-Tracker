77bac769b5ca314587847a33bd706e4bbff33ca1
mbone1 <m.bonnev10@gmail.com>
2020-09-29 16:10:13 -0700

    small modification, attempt 2

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParse: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 
